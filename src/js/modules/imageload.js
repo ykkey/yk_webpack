570a7cfbeac314f7637ff139d9759a5b5095985a
yukiko sano <yukiko.sano@kinto-jp.com>
2020-07-20 16:35:13 +0900

    追加修正

---
+++ src/js/modules/imageload.js
@@ -0,0 +1,41 @@
+// image load 
+// 画像読み込み制御
+// export default cla
+export default class imageLoad {
+  constructor(opts = {}) {
+    this.targetElement = opts.targetElement;
+    this.targetImageUrl = opts.targetImageUrl;
+    var elNum = this.targetElement ? this.targetElement.length : 0;
+    var imgNum = this.targetImageUrl ? this.targetImageUrl.length : 0;
+    this.ALLNUM = elNum + imgNum;
+    this.CNT = 0;
+    this.loadUrlImage();
+    this.callback = opts.callback;
+  }
+
+  loadUrlImage() {
+    if (!this.targetImageUrl.length) return false;
+    for(var i = 0; i < this.targetImageUrl.length; i++) {
+      // var target_img = document.createElement("img")
+      this.loadimage(this.targetImageUrl[i]);
+    }
+  }
+
+  loadimage(target_img) {
+    var img = new Image();
+    img.addEventListener("load", () => {
+      this.counter();
+    },false);
+    img.addEventListener("error", () => {
+      this.counter();
+    },false);
+    img.src = target_img;
+  }
+
+  counter() {
+    this.CNT++;
+    if (this.CNT >= this.ALLNUM) {
+      this.callback();
+    }
+  }
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,26 @@
 import "../sass/style.scss";
 
-import additionCalculator from './modules/addition-calculator';
-// 画像の読み込み
-// import imagesLoadListener from './modules/imagesLoadListener';
+import imageLoad from './modules/imageLoad';
 import scrollFadeIn from './modules/scrollfadein.js';
 
+const PATH_IMG = "/lp/yariscross/assets/img/";
+var pre = window.devicePixelRatio >= 2 ? ((window.innerWidth || document.documentElement.clientWidth || 0) < 835 ? "_sp" : "@2x") : "";
+new imageLoad({
+    targetImageUrl : [
+        PATH_IMG + `mv/bg_fv.jpg`,
+        PATH_IMG + `mv/img_car.png`,
+        PATH_IMG + `mv/txt_title${pre}.png`,
+        PATH_IMG + `mv/txt_debut.png`,
+        PATH_IMG + `mv/bg_scroll.png`,
+        PATH_IMG + `mv/bg_fixed.png`,
+    ],
+    callback: function() {
+        setTimeout(() => {
+            document.getElementsByClassName("p-lpyaris__mv")[0].classList.add("is-lpyaris_fadeIn");
+        },200);
+    }
+});
 
 window.addEventListener('load', function () {
-    console.log("fini load");
     new scrollFadeIn();
 });
